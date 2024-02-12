ee84a666d92975338e621489e7aa9080521da645
Stiwarg <jhonestiveng44@gmail.com>
2024-02-12 16:48:30 -0500

    plantilla

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,82 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library -->
+      <li class="nav-item menu-open">
+        <a href="#" class="nav-link active">
+          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <p>
+            Dashboard
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="./index2.html" class="nav-link active">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Dashboard v2</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-chart-pie"></i>
+          <p>
+            Tablas
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+        
+          <li class="nav-item">
+            <a href="pages/charts/flot.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Aprendices</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/charts/inline.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Contratistas</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/charts/uplot.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Planta</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-edit"></i>
+          <p>
+            Correos
+            <i class="fas fa-angle-left right"></i>
+          </p>
+        </a>
+        <ul class="nav nav-treeview">
+          <li class="nav-item">
+            <a href="pages/forms/editors.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Primer Correo</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/forms/editors.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Segundo Correo</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="pages/forms/editors.html" class="nav-link">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Tercer Correo</p>
+            </a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
