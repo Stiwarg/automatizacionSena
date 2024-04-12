d38b1c6940b7e0b27e0067e5e95d8388f12fc3e0
luisarley <loittrres123@gmail.com>
2024-04-11 22:59:29 -0500

    brujeria

--- app/Http/Controllers/PythonController.php
+++ app/Http/Controllers/PythonController.php
@@ -9,16 +9,11 @@ class PythonController extends Controller
     public function ejecutarPython()
     {
         // Ruta al archivo Python
-<<<<<<< Updated upstream
         // $rutaArchivoPython = 'C:/xampp/htdocs/automatizacionSena/bots/areaPlanta.py';
         // $rutaArchivoPython = 'C:/xampp/htdocs/automatizacionSena/bots/prueba.py';
         // $rutaArchivoPython = 'C:/xampp/htdocs/automatizacionSena/bots/contratistasBaseD.py';
         $rutaArchivoPython = 'C:/xampp/htdocs/automatizacionSena/bots/asignacionResultados.py';
-        #$rutaArchivoPython = 'C:\xampp\htdocs\automatizacionSena\bots\prueba.py';
-=======
-        // $rutaArchivoPython = 'C:/xampp/htdocs/automatizacionSena/bots/EjecucionScripts.py';
-        $rutaArchivoPython = 'C:/xamppTC/htdocs/laravelapp/automatizacionSena/bots/prueba.py';
->>>>>>> Stashed changes
+
 
         // Ejecutar el script Python y capturar la salida completa
         exec("python {$rutaArchivoPython} 2>&1", $output, $returnCode);
